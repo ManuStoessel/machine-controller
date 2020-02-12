781ce81d22c9554efa577dbfd0e29cfb1b440c8b
Manuel Stößel <manuel@loodse.com>
2020-02-12 07:52:01 +0100

    Save changes

--- pkg/cloudprovider/provider/metal3/provider.go
+++ pkg/cloudprovider/provider/metal3/provider.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package hetzner
+package metal3
 
 import (
 	"context"
@@ -32,18 +32,29 @@ import (
 	"github.com/kubermatic/machine-controller/pkg/providerconfig"
 	providerconfigtypes "github.com/kubermatic/machine-controller/pkg/providerconfig/types"
 
-	"k8s.io/apimachinery/pkg/types"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/rest"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+var (
+	dynscheme *runtime.Scheme = runtime.NewScheme()
+)
+
+func init() {
+	// Workaround for fixing this error: 'no kind is registered for the type v1alpha1.BareMetalHost in scheme "k8s.io/client-go/kubernetes/scheme/register.go:65"'
+	metav1.AddToGroupVersion(dynscheme, metal3v1alpha1.SchemeGroupVersion)
+	dynscheme.AddKnownTypes(metal3v1alpha1.SchemeGroupVersion, &metal3v1alpha1.BareMetalHost{}, &metal3v1alpha1.BareMetalHostList{})
+}
+
 const (
-	bmcSecretName string = "metal3-bmh-bmc-credentials"
+	bmcSecretName      string = "metal3-bmh-bmc-credentials"
 	userDataSecretName string = "metal3-bmh-userdata"
 )
 
@@ -57,13 +68,13 @@ func New(configVarResolver *providerconfig.ConfigVarResolver) cloudprovidertypes
 }
 
 type Config struct {
-	BMCAddress 		string
-	BMCUser    		string
-	BMCPassword		string
-	ImageURL		string
-	ImageChecksum	string
-	Kubeconfig      rest.Config
-	Labels     		map[string]string
+	BMCAddress    string
+	BMCUser       string
+	BMCPassword   string
+	ImageURL      string
+	ImageChecksum string
+	Kubeconfig    rest.Config
+	Labels        map[string]string
 }
 
 func (p *provider) getConfig(s v1alpha1.ProviderSpec) (*Config, *providerconfigtypes.Config, error) {
@@ -116,7 +127,7 @@ func (p *provider) getConfig(s v1alpha1.ProviderSpec) (*Config, *providerconfigt
 }
 
 func (p *provider) Validate(spec v1alpha1.MachineSpec) error {
-	c, pc, err := p.getConfig(spec.ProviderSpec)
+	_, _, err := p.getConfig(spec.ProviderSpec)
 	if err != nil {
 		return fmt.Errorf("failed to parse config: %v", err)
 	}
@@ -134,7 +145,7 @@ func (p *provider) Create(machine *v1alpha1.Machine, _ *cloudprovidertypes.Provi
 			Message: fmt.Sprintf("Failed to parse MachineSpec, due to %v", err),
 		}
 	}
-	sigClient, err := client.New(&c.Kubeconfig, client.Options{})
+	sigClient, err := client.New(&c.Kubeconfig, client.Options{Scheme: dynscheme})
 	if err != nil {
 		return nil, fmt.Errorf("failed to get metal3 client: %v", err)
 	}
@@ -142,31 +153,31 @@ func (p *provider) Create(machine *v1alpha1.Machine, _ *cloudprovidertypes.Provi
 
 	bmh := &metal3v1alpha1.BareMetalHost{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      	machine.Name,
-			Namespace: 	machine.Namespace,
-			Labels: 	c.Labels,
+			Name:      machine.Name,
+			Namespace: machine.Namespace,
+			Labels:    c.Labels,
 		},
 		Spec: metal3v1alpha1.BareMetalHostSpec{
 			BMC: metal3v1alpha1.BMCDetails{
-				Address: c.BMCAddress,
+				Address:         c.BMCAddress,
 				CredentialsName: bmcSecretName,
 			},
-			Online: true,
+			Online:                true,
 			ExternallyProvisioned: false,
 			Image: &metal3v1alpha1.Image{
-				URL: c.ImageURL,
+				URL:      c.ImageURL,
 				Checksum: c.ImageChecksum,
 			},
 			Taints: machine.Spec.Taints,
 			UserData: &corev1.SecretReference{
-				Name: userDataSecretName,
+				Name:      userDataSecretName,
 				Namespace: machine.Namespace,
 			},
 			ConsumerRef: &corev1.ObjectReference{
-				Kind: "Machine",
-				Namespace: machine.Namespace,
-				Name: machine.Name,
-				UID: machine.UID, // no idea...
+				Kind:       "Machine",
+				Namespace:  machine.Namespace,
+				Name:       machine.Name,
+				UID:        machine.UID, // no idea...
 				APIVersion: "metal3.io/v1alpha1",
 			},
 		},
@@ -215,7 +226,7 @@ func (p *provider) Cleanup(machine *v1alpha1.Machine, data *cloudprovidertypes.P
 			Message: fmt.Sprintf("Failed to parse MachineSpec, due to %v", err),
 		}
 	}
-	sigClient, err := client.New(&c.Kubeconfig, client.Options{})
+	sigClient, err := client.New(&c.Kubeconfig, client.Options{Scheme: dynscheme})
 	if err != nil {
 		return false, fmt.Errorf("failed to get metal3 client: %v", err)
 	}
@@ -245,14 +256,14 @@ func (p *provider) Get(machine *v1alpha1.Machine, _ *cloudprovidertypes.Provider
 			Message: fmt.Sprintf("Failed to parse MachineSpec, due to %v", err),
 		}
 	}
-	sigClient, err := client.New(&c.Kubeconfig, client.Options{})
+	sigClient, err := client.New(&c.Kubeconfig, client.Options{Scheme: dynscheme})
 	if err != nil {
-		return nil, fmt.Errorf("failed to get kubevirt client: %v", err)
+		return nil, fmt.Errorf("failed to get metal3 client: %v", err)
 	}
 	ctx := context.Background()
 
 	bmh := &metal3v1alpha1.BareMetalHost{}
-	if err := sigClient.Get(ctx, types.NamespacedName{Namespace: c.Namespace, Name: machine.Name}, bmh); err != nil {
+	if err := sigClient.Get(ctx, types.NamespacedName{Namespace: machine.Namespace, Name: machine.Name}, bmh); err != nil {
 		if !kerrors.IsNotFound(err) {
 			return nil, fmt.Errorf("failed to get BareMetalHost %s: %v", machine.Name, err)
 		}
@@ -277,20 +288,28 @@ func (p *provider) MachineMetricsLabels(machine *v1alpha1.Machine) (map[string]s
 	return nil, nil
 }
 
+func (p *provider) SetMetricsForMachines(machines v1alpha1.MachineList) error {
+	return nil
+}
+
 type metal3Host struct {
 	host *metal3v1alpha1.BareMetalHost
 }
 
-func (m *metal3Host) Name() string {
-	return m.host.Name //maybe?
+func (m metal3Host) Name() string {
+	return m.host.Name
 }
 
-func (m *metal3Host) ID() string {
-	return m.host.UID // probably not
+func (m metal3Host) ID() string {
+	return string(m.host.UID)
 }
 
-func (m *metal3Host) Addresses() []string {
+func (m metal3Host) Addresses() []string {
 	var addresses []string
+	if m.host.Status.HardwareDetails.NIC == nil {
+		return addresses
+	}
+
 	for _, nic := range m.host.Status.HardwareDetails.NIC {
 		addresses = append(addresses, nic.IP)
 	}
@@ -298,7 +317,7 @@ func (m *metal3Host) Addresses() []string {
 	return addresses
 }
 
-func (m *metal3Host) Status() instance.Status {
+func (m metal3Host) Status() instance.Status {
 	switch m.host.Status.Provisioning.State {
 	case metal3v1alpha1.StateProvisioning:
 		return instance.StatusCreating
@@ -309,8 +328,4 @@ func (m *metal3Host) Status() instance.Status {
 	default:
 		return instance.StatusUnknown
 	}
-}
-
-func (p *provider) SetMetricsForMachines(machines v1alpha1.MachineList) error {
-	return nil
 }
